ec3d45255be2e5d78ce95248de2edfe4c69617d5
terricom <mo0922@gmail.com>
2023-03-22 21:08:39 +0800

    Local Variables

---
+++ contracts/LocalVariables.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.19;
+
+contract LocalVariables {
+    uint public i;
+    bool public b;
+    address public myAddress;
+
+    function foo() external {
+        uint x = 123;
+        bool f = false;
+
+        x += 456;
+        f = true;
+
+        i = 123;
+        b = true;
+        myAddress = address(1);
+    }
+}
