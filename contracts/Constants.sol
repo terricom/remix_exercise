27e5279718cd66b88587e1a7094ff474a08e591b
terricom <mo0922@gmail.com>
2023-03-22 21:27:00 +0800

    Contracts

---
+++ contracts/Constants.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract Constants {
+    address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+    uint public constant MY_UINT = 123;
+}
+
+contract Var {
+    address public MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+}
