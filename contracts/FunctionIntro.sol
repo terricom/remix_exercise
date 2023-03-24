023a4243389d3f7fdbdc533128658581e3e90b29
terricom <mo0922@gmail.com>
2023-03-24 20:30:43 +0800

    FunctionIntro

---
+++ contracts/FunctionIntro.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract FinctionIntro {
+    function add(uint x, uint y) external pure returns (uint) {
+        return x + y;
+    }
+
+    function sub(uint x, uint y) external pure returns (uint) {
+        return x - y;
+    }
+}
