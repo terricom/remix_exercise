b6723d4f2fab3a1fd8ffeaade54e588c292f6ad9
terricom <mo0922@gmail.com>
2023-04-11 19:47:18 +0800

    Fallback

---
+++ contracts/Fallback.sol
@@ -0,0 +1,34 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/*
+Fallback executed when
+- function doesn't exist
+- directly send Eth
+
+fallback()? or receive()?
+
+    Ether is sent to contract
+                |
+        is msg.data empty?
+               / \
+             yes  no
+             /      \
+receive() exists?   fallback()
+          /   \
+        yes    no
+        /        \
+    receive()   fallback()
+*/
+
+contract Fallback {
+    event Log(string func, address sender, uint value, bytes data);
+
+    fallback() external payable {
+        emit Log("fallback", msg.sender, msg.value, msg.data);
+    }
+
+    receive() external payable {
+        emit Log("receive", msg.sender, msg.value, "");
+    }
+}
